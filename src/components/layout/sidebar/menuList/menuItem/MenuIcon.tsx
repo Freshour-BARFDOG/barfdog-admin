73a1136237b395e1944226a23137f836b740a3fb
jkeFreshour <jke@freshour.co.kr>
2025-06-23 10:41:12 +0900

    feat: menuItem icon type 수정

--- src/components/layout/sidebar/menuList/menuItem/MenuIcon.tsx
+++ src/components/layout/sidebar/menuList/menuItem/MenuIcon.tsx
@@ -1,13 +1,14 @@
-import { ComponentClass, SVGProps } from "react";
+import { ComponentType, SVGProps } from "react";
 import SvgIcon from "@/components/common/svgIcon/SvgIcon";
 
 interface MenuIconProps {
 	active: boolean;
-	icon: ComponentClass<SVGProps<SVGSVGElement>>;
+	icon?: ComponentType<SVGProps<SVGSVGElement>>;
 	size?: number;
 }
 
 const MenuIcon = ({ active, icon, size }: MenuIconProps) => {
+	if(!icon) return null;
 	return (
 		<SvgIcon src={icon} size={size} color={active ? 'red' : 'gray900'} />
 	);
