6b7068b16dc2f35e7538e6ae27862b706f26c7fb
jaehyeon <wjh5296@gmail.com>
2025-08-01 16:36:55 +0900

    feat: 로그인 컴포넌트 모바일 고려해서 수정

--- src/components/pages/auth/login/Login.css.ts
+++ src/components/pages/auth/login/Login.css.ts
@@ -2,34 +2,34 @@ import { style } from "@vanilla-extract/css";
 import { themeVars } from "@/styles/theme.css";
 
 export const loginContainer = style({
-	width: '100vw',
-	height: '100vh',
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-	backgroundColor: themeVars.colors.gray.gray50,
-})
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: themeVars.colors.gray.gray50,
+});
 
-export const loginCard = style({
-	width: '30% !important',
-})
+export const loginCardWrapper = style({
+  width: "400px",
+});
 
 export const loginLogo = style({
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-})
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+});
 
 export const loginForm = style({
-	width: '100%',
-	display: 'flex',
-	flexDirection: 'column',
-	gap: '12px',
-})
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  gap: "12px",
+});
 
 export const loginControls = style({
-	width: '100%',
-	display: 'flex',
-	justifyContent: 'space-between',
-	alignItems: 'center',
-})
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
