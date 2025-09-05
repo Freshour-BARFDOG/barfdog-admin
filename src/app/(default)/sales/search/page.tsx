c5bef0ea546c4b52b4c73a085a98e85b12a32556
jaehyeon <wjh5296@gmail.com>
2025-09-05 13:34:47 +0900

    feat: 판매관리, 구독 히스토리, 반려견 관리 경로에 loading.tsx 추가

--- src/app/(default)/sales/search/page.tsx
+++ src/app/(default)/sales/search/page.tsx
@@ -1,17 +1,17 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Wrapper from "@/components/layout/wrapper/Wrapper";
-import Loader from "@/components/common/loader/Loader";
 import SalesSearch from "@/components/pages/sales/search/SalesSearch";
+import Spinner from "@/components/common/spinner/Spinner";
 
 export const metadata = {
-  title: '관리자 | 주문 통합 검색',
+  title: "관리자 | 주문 통합 검색",
 };
 
 export default async function SalesSearchPage() {
   return (
     <ErrorBoundary fallback={<div>주문 통합 검색 정보가 없습니다.</div>}>
-      <Suspense fallback={<Loader fullscreen />}>
+      <Suspense fallback={<Spinner fullscreen />}>
         <Wrapper title="통합 검색">
           <SalesSearch />
         </Wrapper>
